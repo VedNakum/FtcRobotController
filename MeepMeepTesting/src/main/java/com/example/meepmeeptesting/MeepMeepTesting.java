4590c9b1b139fa1875502f03389fc8b21e25f19b
VedNakum <113805164+VedNakum@users.noreply.github.com>
2024-09-09 15:43:18 -0400

    3

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,34 +6,56 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(65, 30, -85))
-                .splineTo(new Vector2d(10, 25), Math.toRadians(-90))
-                .waitSeconds(5)
-                .splineTo(new Vector2d(40, 40), Math.toRadians(180))
-                .strafeTo(new Vector2d(40, 57))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -34, Math.toRadians(180)))
+                .splineToConstantHeading(new Vector2d(34,-10),Math.toRadians(180))
                 .waitSeconds(2)
-                .splineTo(new Vector2d(50, 56), Math.toRadians(-25))
+                .lineToX(40)
+                .splineToConstantHeading(new Vector2d(38,-45),Math.toRadians(180))
                 .waitSeconds(2)
-                .splineTo(new Vector2d(40, 53), Math.toRadians(180))
-                .lineToX(37)
+                .splineTo(new Vector2d(53, -50), Math.toRadians(310))
                 .waitSeconds(2)
-                .splineTo(new Vector2d(50, 56), Math.toRadians(-25))
-                .strafeTo(new Vector2d(55, 67))
+
+                .splineTo(new Vector2d(38, -55), Math.toRadians(180))
+                .waitSeconds(2)
+                .splineTo(new Vector2d(53, -50), Math.toRadians(310))
+                .waitSeconds(2)
+                .splineTo(new Vector2d(24, -55), Math.toRadians(270))
+                .waitSeconds(2)
+                .lineToY(-50)
+                .splineTo(new Vector2d(53, -50), Math.toRadians(130))
+
+
+
+
+
+                //.setTangent(Math.toRadians(90))
+                //.lineToY(55)
+                //.setTangent(Math.toRadians(0))
+                //.lineToX(32)
+                //.setTangent(Math.toRadians(0))
+                //.splineTo(new Vector2d(44,28), Math.toRadians(0))
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.25f)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .start();
+//  <following code you were using previously>
+
     }
 }
