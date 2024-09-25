1ca4c61083c20f6d48a265d66131b6df6f8fe8be
VedNakum <113805164+VedNakum@users.noreply.github.com>
2024-09-25 19:06:10 -0400

    h

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -18,32 +18,33 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(75, 75, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -34, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(34,-10),Math.toRadians(180))
-                .waitSeconds(2)
-                .lineToX(40)
-                .splineToConstantHeading(new Vector2d(38,-45),Math.toRadians(180))
-                .waitSeconds(2)
-                .splineTo(new Vector2d(53, -50), Math.toRadians(310))
-                .waitSeconds(2)
-
-                .splineTo(new Vector2d(38, -55), Math.toRadians(180))
-                .waitSeconds(2)
-                .splineTo(new Vector2d(53, -50), Math.toRadians(310))
-                .waitSeconds(2)
-                .splineTo(new Vector2d(24, -55), Math.toRadians(270))
-                .waitSeconds(2)
-                .lineToY(-50)
-                .splineTo(new Vector2d(53, -50), Math.toRadians(130))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-38, -61, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(-10,-40),Math.toRadians(180))
+                        .waitSeconds(3)
+                .splineToConstantHeading(new Vector2d(-48,-42),Math.toRadians(180))
+                        .waitSeconds(3)
+                .splineTo(new Vector2d(-52,-52),Math.toRadians(-140))
+                        .waitSeconds(2)
+                .splineTo(new Vector2d(-57,-37),Math.toRadians(90))
+                        .waitSeconds(2)
+                .splineTo(new Vector2d(-52,-52),Math.toRadians(-140))
+                        .waitSeconds(2)
+                .splineTo(new Vector2d(-52,-25),Math.toRadians(-180))
+                        .waitSeconds(2)
+                .splineTo(new Vector2d(-52,-52),Math.toRadians(-140))
 
 
 
 
 
-                //.setTangent(Math.toRadians(90))
+
+
+
+
+                //
                 //.lineToY(55)
                 //.setTangent(Math.toRadians(0))
                 //.lineToX(32)
